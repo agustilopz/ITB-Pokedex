3437e1f4a6b3af5c05f138aa7d641d95be013c32
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-02-28 10:41:53 +0100

    create project

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,9 @@
+module org.itb.m3a54pokemonagustilopez {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires kotlin.stdlib;
+
+
+    opens org.itb.m3a54pokemonagustilopez to javafx.fxml;
+    exports org.itb.m3a54pokemonagustilopez;
+}
