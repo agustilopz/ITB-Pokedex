f7a35285a5eed9f8cc538a04044978cbd3a0c445
agusti.lopez <agusti.lopez.7e8@itb.cat>
2025-03-02 21:37:49 +0100

    GUI display

--- src/main/kotlin/org/itb/m3a54pokemonagustilopez/HelloApplication.kt
+++ src/main/kotlin/org/itb/m3a54pokemonagustilopez/HelloApplication.kt
@@ -8,7 +8,7 @@ import javafx.stage.Stage
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 420.0, 240.0)
+        val scene = Scene(fxmlLoader.load(), 500.0, 300.0)
         stage.title = "Pokémon Viewer"
         stage.scene = scene
         stage.show()
--- src/main/kotlin/org/itb/m3a54pokemonagustilopez/HelloController.kt
+++ src/main/kotlin/org/itb/m3a54pokemonagustilopez/HelloController.kt
@@ -58,11 +58,40 @@ class HelloController {
     private lateinit var lblWeight: Label
 
     @FXML
+    private lateinit var lblEncounters1: Label
+
+    @FXML
+    private lateinit var lblEncounters2: Label
+
+    @FXML
+    private lateinit var lblEncounters3: Label
+
+    @FXML
+    private lateinit var progressBarHp: ProgressBar
+
+    @FXML
+    private lateinit var progressBarAttack: ProgressBar
+
+    @FXML
+    private lateinit var progressBarDefense: ProgressBar
+
+    @FXML
+    private lateinit var progressBarSpAtk: ProgressBar
+
+    @FXML
+    private lateinit var progressBarSpDef: ProgressBar
+
+    @FXML
+    private lateinit var progressBarSpeed: ProgressBar
+
+    @FXML
     private lateinit var btnPrevious: Button
 
     // Començarem pel pokemon en la posició [0]
     private var indexActual = 0
     var llistaPokemons = crearPokemons()
+
+
 
     @FXML
     fun initialize() {
@@ -74,14 +103,22 @@ class HelloController {
         val pokemon = llistaPokemons[indexActual]
         lblCount.text = "${indexActual+1}/${llistaPokemons.size}"
         lblName.text = "Name: " + pokemon.getName()
-        lblType1.text = "Type 1: " + pokemon.getType1().eng
-        lblType2.text = "Type 2: " + pokemon.getType2().eng
+        lblType1.text = "Type 1: " + pokemon.getType1()
+        lblType2.text = "Type 2: " + pokemon.getType2()
         lblHp.text = "HP: " + pokemon.getHp()
         lblAttack.text = "Attack: " + pokemon.getAttack()
         lblDefense.text = "Defense: " + pokemon.getDefense()
         lblspAtk.text = "Sp. Atk: " + pokemon.getSpatk()
         lblspDef.text = "Sp. Def: " + pokemon.getSpdef()
         lblspeed.text = "Speed: " + pokemon.getSpeed()
+        lblEncounters1.text = "Encounters: "
+        lblEncounters2.text ="Number Defeated: 0"
+        lblEncounters3.text ="Number Defeated: 0"
+        progressBarAttack.progress = pokemon.getHp() / 100.0
+        progressBarDefense.progress = pokemon.getDefense() / 100.0
+        progressBarSpAtk.progress = pokemon.getSpatk() / 100.0
+        progressBarSpDef.progress = pokemon.getSpdef() / 100.0
+
     }
     @FXML
     fun previousPokemon() {
